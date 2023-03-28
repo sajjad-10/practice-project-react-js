26c6e313295a95e7bbb887603efef8bfe5008651
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-29 00:02:41 +0330

    header

--- shopping/components/Layout.js
+++ shopping/components/Layout.js
@@ -1,12 +1,23 @@
 import Head from "next/head";
+import Link from "next/Link";
 export default function Layout({ children, title }) {
     return (
         <>
             <Head>
                 <title>{`${title} - Shopping`}</title>
             </Head>
-            <div>
-                <header>header</header>
+            <div className="flex min-h-screen flex-col justify-between">
+                <header>
+                    <nav className="flex h-14 px-8 justify-between items-center border-b-4">
+                        <Link href="/">
+                            <span className="text-lg font-bold">Shopping</span>
+                        </Link>
+                        <div>
+                        <Link href="/cart" className="p-2">Cart</Link>
+                        <Link href="/login" className="p-2">login</Link>
+                        </div>
+                    </nav>
+                </header>
                 <main>{children}</main>
                 <footer>footer</footer>
             </div>
