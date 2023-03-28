bcfa59dbe40c031378653a7e239baa7e227caea7
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-29 00:08:55 +0330

    main bg

--- shopping/pages/_app.js
+++ shopping/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <div className="bg-gray-100 text-black">
+                <Component {...pageProps} />
+            </div>
+        </>
+    );
 }
