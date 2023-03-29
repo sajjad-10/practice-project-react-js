0157fb561eb1cbf5126a617f8bd9dd2f40069e34
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-29 10:15:38 +0330

    show prodouct on home page

--- shopping/pages/index.js
+++ shopping/pages/index.js
@@ -1,15 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Layout from "../components/Layout";
+import Product from "../components/Product";
 
-const inter = Inter({ subsets: ["latin"] });
+import productItems from "../data/products.json";
 
 export default function Home() {
     return (
         <>
-            <Layout title="Home page">Home page</Layout>
+            <Layout title="Home page">
+                <div className="grid grid-cols-1 gap-12 md:grid-cols-3 lg:grid-cols-4">
+                    {productItems.map((pItem) => (
+                        <Product item={pItem} key={pItem.slug} />
+                    ))}
+                </div>
+            </Layout>
         </>
     );
 }
