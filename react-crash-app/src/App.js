b24b3b90b00d293f96d2b2c9fb723191601d6856
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-30 12:24:36 +0330

    state initial-state

--- react-crash-app/src/App.js
+++ react-crash-app/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./components/Product/Product";
 
 const App = () => {
+    const [productState, setProductState] = useState({
+        product: [
+            { title: "Book 1", price: 99 },
+            { title: "Book 2", price: 88 },
+        ],
+    });
     return (
         <div id="main" className="container">
-            <Product title="Book 1" price="30">
+            <Product
+                title={productState.product[0].title}
+                price={productState.product[0].price}
+            >
                 Discount: 20%
             </Product>
+            <Product
+                title={productState.product[1].title}
+                price={productState.product[1].price}
+            />
         </div>
     );
 };
