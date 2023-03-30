f150632fa9194fd7f7c567fb7e403e4c02ff9d26
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-03-30 08:31:07 +0330

    use moudul in componnet

--- react-crash-app/src/index.js
+++ react-crash-app/src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from './App'
 
-const App = () => {
-    return React.createElement(
-        "div",
-        { id: "main", className: "container" },
-        "Hello World"
-    );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
