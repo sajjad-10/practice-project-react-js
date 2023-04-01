aa899e8fb540e7bd463aefee6c7800c9f47f9494
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-01 18:54:52 +0330

    Shopping

--- shopping-react-app/src/App.js
+++ shopping-react-app/src/App.js
@@ -1,9 +1,13 @@
 import Layout from "./components/Layout/Layout";
+import Shopping from "./containers/Shopping/Shopping";
 
 function App(props) {
     return (
         <div>
-            <Layout><p>Shopping APP</p></Layout>
+            <Layout>
+                <p>Shopping APP</p>
+                <Shopping />
+            </Layout>
         </div>
     );
 }
---
+++ shopping-react-app/src/containers/Shopping/Shopping.js
@@ -0,0 +1,11 @@
+import Wrapper from "../../hoc/Wrapper";
+
+const Shopping = () => {
+    return (
+        <Wrapper>
+            <div>Controls</div>
+        </Wrapper>
+    );
+};
+
+export default Shopping;
