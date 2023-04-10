4ea9d0d07c3ef96c24588c37c6261c1a5897ed03
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-10 10:17:34 +0330

    navigating to dynamic routes

--- next-crash-app/pages/users/index.js
+++ next-crash-app/pages/users/index.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 const UsersPage = () => {
+    const users = [
+        { id: 1, userName: "Sajjad" },
+        { id: 2, userName: "Admin" },
+    ];
     return (
         <div>
             <h2>UsersPage</h2>
+            <div>
+                <ul>
+                    {users.map((item) => (
+                        <li key={item.id}>
+                            <Link href={`/users/${item.id}`}>
+                                {item.userName}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
