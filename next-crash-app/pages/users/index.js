acc2d210827ff41a3a1d4d8f341dd39dfbc219db
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-10 08:03:02 +0330

    static route

---
+++ next-crash-app/pages/users/index.js
@@ -0,0 +1,9 @@
+const UsersPage = () => {
+    return (
+        <div>
+            <h2>UsersPage</h2>
+        </div>
+    );
+};
+
+export default UsersPage;
---
+++ next-crash-app/pages/users/orders.js
@@ -0,0 +1,9 @@
+const OrdersPage = () => {
+    return (
+        <div>
+            <h2>OrdersPage</h2>
+        </div>
+    );
+};
+
+export default OrdersPage;
