9af6b5796fe73a8066e9ffe934298d553defec1b
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-10 08:58:43 +0330

    useRouter pathname - query

--- next-crash-app/pages/users/[id].js
+++ next-crash-app/pages/users/[id].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router";
 const userId = () => {
+    const router = useRouter()
+    
+    console.log(router.pathname);
+    console.log(router.query);
     return (
         <div>
             <h2>User ID</h2>
