88ba081daf4f12d17fa94fee356c2f3cd2d66754
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-11 05:13:30 +0330

    routing white button

--- next-crash-app/pages/users/[id].js
+++ next-crash-app/pages/users/[id].js
@@ -4,9 +4,19 @@ const userId = () => {
     
     console.log(router.pathname);
     console.log(router.query);
+
+    const showUserIdHandler = ()=>{
+        // router.push('/users/sajjad')
+
+        router.push({
+            pathname : '/users/[id]',
+            query : {id : 'sajjad'}
+        })
+    }
     return (
         <div>
             <h2>User ID</h2>
+            <button onClick={showUserIdHandler}>Show User ID (Page)</button>
         </div>
     );
 };
