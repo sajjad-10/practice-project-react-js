056aec95d134819694bd02b09cd601185cf530de
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-30 09:17:09 +0330

    components in react code

---
+++ react-crash-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return React.createElement(
+    "div",
+    { id: "main", className: "container" },
+    "Hello React"
+);
+}
+ 
+export default App;
--- react-crash-app/src/index.js
+++ react-crash-app/src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const element = React.createElement(
-    "div",
-    { id: "main", className: "container" },
-    "Hello React"
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<App />);
