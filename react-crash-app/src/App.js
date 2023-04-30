00cce9c3274c031cfd018e446d4d032c10153c2f
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-30 21:40:56 +0330

    change state

--- react-crash-app/src/App.js
+++ react-crash-app/src/App.js
@@ -10,7 +10,12 @@ const App = () => {
     const [products, setProducts] = useState(data);
 
     const changePriceHandler = () => {
-        console.log("Clicked!");
+        const data = [
+            { title: "Book 1", price: 30.99 },
+            { title: "Book 2", price: 40.99 },
+            { title: "Book 3", price: 50.99, discount: 20 },
+        ];
+        setProducts(data)
     };
 
     return (
