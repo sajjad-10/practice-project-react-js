87ad12e1f45cf764cd747bea76f3db8f037dd75d
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-04-30 09:40:09 +0330

    using component

--- react-crash-app/src/App.js
+++ react-crash-app/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Product from "./components/Product/Product";
 
 const App = () => {
     return (
         <div id="main" className="container">
             <h2>Hello React</h2>
+            <Product />
         </div>
     );
 };
