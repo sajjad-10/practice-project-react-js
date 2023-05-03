56f722e53a0de9abf61a7bc3807d85c62002627a
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-05-03 20:50:18 +0330

    conditionally rendering

--- react-crash-app/src/App.js
+++ react-crash-app/src/App.js
@@ -9,14 +9,10 @@ const App = () => {
         { title: "Book 3", price: 40.99, discount: 20 },
     ];
     const [products, setProducts] = useState(data);
+    const [showProducts, setShowProducts] = useState(false);
 
-    const changePriceHandler = (newTitle) => {
-        const data = [
-            { title: newTitle, price: 30.99 },
-            { title: "Book 2", price: 40.99 },
-            { title: "Book 3", price: 50.99, discount: 20 },
-        ];
-        setProducts(data);
+    const toggleProductHandler = () => {
+        setShowProducts(!showProducts);
     };
     const changeTitleHandler = (event) => {
         const data = [
@@ -27,30 +23,35 @@ const App = () => {
         setProducts(data);
     };
     const btn = {
-        backgroundColor: '#7b1fa2',
-        color: '#fff',
-        font: 'inherit',
-        border: 'none',
-        outline: 'none',
-        borderRadius: '3px',
-        padding: '0.6rem',
-        margin: '0.6rem auto',
-    }
+        backgroundColor: "#7b1fa2",
+        color: "#fff",
+        font: "inherit",
+        border: "none",
+        outline: "none",
+        borderRadius: "3px",
+        padding: "0.6rem",
+        margin: "0.6rem auto",
+    };
 
     return (
         <div id="main" className="center">
             <h2>Book Store</h2>
-            {products.map((item) => (
-                <Product
-                    title={item.title}
-                    price={item.price}
-                    click={() => changePriceHandler("New Title")}
-                    change={changeTitleHandler}
-                >
-                    {item.discount}%
-                </Product>
-            ))}
-            <button style={btn} onClick={changePriceHandler}>Change Price</button>
+            <button style={btn} onClick={toggleProductHandler}>
+                Show/Hide Products
+            </button>
+            {showProducts ? (
+                <div>
+                    {products.map((item) => (
+                        <Product
+                            title={item.title}
+                            price={item.price}
+                            change={changeTitleHandler}
+                        >
+                            {item.discount}%
+                        </Product>
+                    ))}
+                </div>
+            ) : null}
         </div>
     );
 };
