b722516e215888e6f84703964dd37d9e4f8e94a8
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-05-08 11:19:57 +0430

    lifecycle in func

--- react-crash-app/src/components/Main/Main.js
+++ react-crash-app/src/components/Main/Main.js
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
 const Main = (props) => {
+    useEffect(() => {
+        console.log("Main.js useEffect");
+    });
     const btn = {
         backgroundColor: "#7b1fa2",
         color: "#fff",
