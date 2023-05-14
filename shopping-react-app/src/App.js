d0f72807633eefb7ea18140d363e41f9567ba11c
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-05-14 09:55:30 +0330

    creating Layout & Wrapper components

--- shopping-react-app/src/App.js
+++ shopping-react-app/src/App.js
@@ -1,5 +1,13 @@
+import Layout from "./components/Layout/Layout";
+
 const App = () => {
-    return ( <>Hello</> );
-}
- 
+    return (
+        <div>
+            <Layout>
+                <p>Shopping App</p>
+            </Layout>
+        </div>
+    );
+};
+
 export default App;
