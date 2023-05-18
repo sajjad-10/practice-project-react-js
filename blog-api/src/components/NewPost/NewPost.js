afe353401adf481277ae0aab3da034429d7e08fa
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-05-18 11:16:16 +0330

    change input value

--- blog-api/src/components/NewPost/NewPost.js
+++ blog-api/src/components/NewPost/NewPost.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
 import "./NewPost.css";
 const NewPost = () => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [author, setAuthor] = useState("Sajjad");
     return (
         <>
             <div className="new-post">
                 <h2>Add a Post</h2>
                 <label>Title</label>
-                <input type="text" />
+                <input
+                    type="text"
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                />
                 <label>Content</label>
-                <textarea rows="4" />
+                <textarea
+                    rows="4"
+                    value={content}
+                    onChange={(event) => setContent(event.target.value)}
+                />
                 <label>Author</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(event) => setAuthor(event.target.value)}
+                >
                     <option value="Sajjad">Sajjad</option>
-                    <option value="Sajjad2">Sajjad-2</option>
                 </select>
                 <button>Add Post</button>
             </div>
