939ea5fbde797b34463fd980d6a131e4bcc0b013
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-06-08 18:16:37 +0330

    loading paths dynamical

--- next-crash-app/pages/[pid].js
+++ next-crash-app/pages/[pid].js
@@ -15,13 +15,18 @@ const ProductDetailPage = (props) => {
     );
 };
 
+async function getData() {
+    const filePath = path.join(process.cwd(), "data", "products.json");
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+    return data;
+}
+
 export async function getStaticProps(context) {
     const { params } = context;
     const productsId = params.pid;
 
-    const filePath = path.join(process.cwd(), "data", "products.json");
-    const jsonData = await fs.readFile(filePath);
-    const data = JSON.parse(jsonData);
+    const data = await getData();
 
     const product = data.products.find((item) => {
         return item.id === productsId;
@@ -31,12 +36,12 @@ export async function getStaticProps(context) {
     };
 }
 export async function getStaticPaths() {
+    const data = await getData();
+    const ids = data.products.map((item) => item.id);
+    const params = ids.map((item) => ({ params: { pid: item } }));
+    
     return {
-        paths: [
-            { params: { pid: "p1" } },
-            { params: { pid: "p2" } },
-            // { params: { pid: "p3" } },
-        ],
+        paths: params,
         fallback: true,
     };
 }
