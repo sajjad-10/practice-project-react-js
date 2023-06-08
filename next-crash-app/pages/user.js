702ac032b08e768d6cca2b3a75275578d4f47307
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-06-08 18:48:16 +0330

    context in getServerSideProps

--- next-crash-app/pages/user.js
+++ next-crash-app/pages/user.js
@@ -5,6 +5,10 @@ function UserPage(props){
 export default UserPage
 
 export async function getServerSideProps(context){
+    const {params, req, res} = context
+
+    console.log(req);
+    console.log(res);
     return{
         props:{username:"Sajjad"}
     }
