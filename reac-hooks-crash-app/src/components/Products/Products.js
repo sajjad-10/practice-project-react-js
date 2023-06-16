149275181b60dad94825de16d8125ef4dd1f47c3
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-06-16 21:30:30 +0330

    sending http req POST

--- reac-hooks-crash-app/src/components/Products/Products.js
+++ reac-hooks-crash-app/src/components/Products/Products.js
@@ -5,12 +5,21 @@ import ProductList from "./ProductList";
 
 function Products() {
     const [products, setProducts] = useState([]);
+
     const addProductHandler = (newProduct) => {
-        setProducts((prevState) => {
-            return [
-                ...prevState,
-                { id: Math.random().toString(), ...newProduct },
-            ];
+        fetch(`${process.env.REACT_APP_FIREBASE_DATABASE_URL}/products.json`, {
+            method: "POST",
+            body: JSON.stringify(newProduct),
+            headers: { "Content-Type": "application/json" },
+        }).then((response) => {
+            response.json().then((responseData) => {
+                setProducts((prevState) => {
+                    return [
+                        ...prevState,
+                        { id: responseData.name, ...newProduct },
+                    ];
+                });
+            });
         });
     };
     return (
