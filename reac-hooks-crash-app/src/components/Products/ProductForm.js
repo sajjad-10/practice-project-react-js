f10bf00037210020ddbc098775d889bc5475e7f0
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-06-16 18:42:16 +0330

    useing useState & update-state

--- reac-hooks-crash-app/src/components/Products/ProductForm.js
+++ reac-hooks-crash-app/src/components/Products/ProductForm.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 
 import "./ProductForm.css";
 
 const ProductForm = React.memo((props) => {
+    const inputState = useState({ title: "", amount: "" });
+
     const submitHandler = (event) => {
         event.preventDefault();
+        console.log(inputState);
     };
 
     return (
@@ -15,11 +18,32 @@ const ProductForm = React.memo((props) => {
                 <form onSubmit={submitHandler}>
                     <div className="form-control">
                         <label htmlFor="title">عنوان</label>
-                        <input type="text" id="title" />
+                        <input
+                            type="text"
+                            id="title"
+                            value={inputState[0].title}
+                            onChange={(event) =>
+                                inputState[1]({
+                                    title: event.target.value,
+                                    amount: inputState[0].amount,
+                                })
+                            }
+                        />
                     </div>
                     <div className="form-control">
                         <label htmlFor="amount">تعداد</label>
-                        <input type="number" id="amount" />
+                        <input
+                            type="number"
+                            id="amount"
+                            value={inputState[0].amount}
+                            onChange={(event) =>
+                                inputState[1]({
+                                    title: inputState[0].title,
+                                    amount: event.target.value,
+                                })
+                            }
+
+                        />
                     </div>
                     <div className="product-form__actions">
                         <button type="submit">افزودن</button>
