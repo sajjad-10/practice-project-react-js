a0a6e040ca2c7cfb4df67ce32823ce0505aea97b
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-06-19 07:11:34 +0330

    useEffect loading-data

--- reac-hooks-crash-app/src/components/Products/Products.js
+++ reac-hooks-crash-app/src/components/Products/Products.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProductForm from "./ProductForm";
 import Search from "./Search";
 import ProductList from "./ProductList";
 
 function Products() {
     const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_FIREBASE_DATABASE_URL}/products.json`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((responseData) => {
+                const loadedProducts = [];
+                for (const item in responseData) {
+                    loadedProducts.push({
+                        id: item,
+                        title: responseData[item].title,
+                        amount: responseData[item].amount,
+                    });
+                }
+                setProducts(loadedProducts);
+            });
+    }, []);
 
     const addProductHandler = (newProduct) => {
         fetch(`${process.env.REACT_APP_FIREBASE_DATABASE_URL}/products.json`, {
