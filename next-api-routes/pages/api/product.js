ce57e0ef631debff8df3b063b14ab243dfcf24e9
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-16 20:28:36 +0330

    save data to file

--- next-api-routes/pages/api/product.js
+++ next-api-routes/pages/api/product.js
@@ -1,13 +1,18 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
     if (req.method === "POST") {
         const title = req.body.title;
         const price = req.body.price;
 
         const newProduct = { title, price };
-        const products = []
-        products.push(newProduct)
-        
-        console.log(products);
+
+        const filePath = path.join(process.cwd(), "data", "products.json");
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData)
+        data.push(newProduct)
+        fs.writeFileSync(filePath, JSON.stringify(data))
     }
 }
 
