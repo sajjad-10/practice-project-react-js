394fe1a1bf403e72bc0a8c382c4137b924471bdc
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-16 19:05:20 +0330

    writing api route

--- next-api-routes/pages/api/product.js
+++ next-api-routes/pages/api/product.js
@@ -1,5 +1,14 @@
 function handler(req, res) {
-    res.json({ message: "Product Api" });
+    if (req.method === "POST") {
+        const title = req.body.title;
+        const price = req.body.price;
+
+        const newProduct = { title, price };
+        const products = []
+        products.push(newProduct)
+        
+        console.log(products);
+    }
 }
 
 export default handler;
