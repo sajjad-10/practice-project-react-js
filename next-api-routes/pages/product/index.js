60763006cd7bf541a469b0f6ae21992e9994d815
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-19 08:32:20 +0330

    handle display on front

--- next-api-routes/pages/product/index.js
+++ next-api-routes/pages/product/index.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import fs from "fs";
 import path from "path";
 
 const ProductPage = (props) => {
+    const [productData, setProductData] = useState();
+    async function productPriceHandler(id) {
+        const response = await fetch(`/api/${id}`);
+        const responseData = await response.json();
+        setProductData(responseData.product)
+    }
     return (
         <>
             <div>
+                {productData && <p>{productData.price}</p>}
                 <ul>
                     {props.productItems.map((item, index) => (
-                        <li key={index}>{item.title}</li>
+                        <li key={index}>
+                            {item.title}
+                            <button
+                                onClick={productPriceHandler.bind(
+                                    null,
+                                    item.id
+                                )}
+                            >
+                                Show Price
+                            </button>
+                        </li>
                     ))}
                 </ul>
             </div>
