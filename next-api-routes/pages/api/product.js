93dfeeedfa397399ff1250c3dfd6e3f92047a34c
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-20 18:44:42 +0330

    insert to db

--- next-api-routes/pages/api/product.js
+++ next-api-routes/pages/api/product.js
@@ -1,24 +1,18 @@
-import fs from "fs";
-import path from "path";
+import {MongoClient} from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === "POST") {
-        const title = req.body.title;
-        const price = req.body.price;
+        const {title, price} = req.body
 
-        const newProduct = { title, price };
-
-        const filePath = path.join(process.cwd(), "data", "products.json");
-        const fileData = fs.readFileSync(filePath);
-        const data = JSON.parse(fileData);
-        data.push(newProduct);
-        fs.writeFileSync(filePath, JSON.stringify(data));
+        const client = await MongoClient.connect('mongodb://127.0.0.1:27017/shop')
+        const db = client.db()
+        await db.collection("products").insertOne({title, price})
     } else {
-        const filePath = path.join(process.cwd(), "data", "products.json");
-        const fileData = fs.readFileSync(filePath);
-        const data = JSON.parse(fileData);
+        // const filePath = path.join(process.cwd(), "data", "products.json");
+        // const fileData = fs.readFileSync(filePath);
+        // const data = JSON.parse(fileData);
 
-        res.json({ products: data });
+        // res.json({ products: data });
     }
 }
 
