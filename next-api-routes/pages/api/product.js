4051783745aaba90071cd58b4eae30374da3eeea
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-20 19:22:15 +0330

    fetch data from db

--- next-api-routes/pages/api/product.js
+++ next-api-routes/pages/api/product.js
@@ -1,18 +1,16 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
+    const client = await MongoClient.connect("mongodb://127.0.0.1:27017/shop");
+    const db = client.db();
+
     if (req.method === "POST") {
-        const {title, price} = req.body
+        const { title, price } = req.body;
 
-        const client = await MongoClient.connect('mongodb://127.0.0.1:27017/shop')
-        const db = client.db()
-        await db.collection("products").insertOne({title, price})
+        await db.collection("products").insertOne({ title, price });
     } else {
-        // const filePath = path.join(process.cwd(), "data", "products.json");
-        // const fileData = fs.readFileSync(filePath);
-        // const data = JSON.parse(fileData);
-
-        // res.json({ products: data });
+        const data = await db.collection("products").find().toArray();
+        res.json({ products: data });
     }
 }
 
