f79e3c4f0ba9a0bf3ebe1c2c079376f512dbe5b9
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-21 10:49:07 +0330

    Header & Footer

--- shopping-next-app/components/Layout.js
+++ shopping-next-app/components/Layout.js
@@ -1,14 +1,23 @@
 import Head from "next/head";
-const Layout = ({children, title}) => {
+import Link from "next/link";
+const Layout = ({ children, title }) => {
     return (
         <>
             <Head>
                 <title>{`${title} - Shopping`}</title>
             </Head>
-            <div>
-                <header>header</header>
-                <main>{children}</main>
-                <footer>footer</footer>
+            <div className="flex min-h-screen flex-col justify-between">
+                <header>
+                    <nav className="flex h-14 px-8 justify-between items-center border-b-4 bg-white">
+                        <Link href={"/"} className="text-lg font-bold">Shopping</Link>
+                        <div>
+                            <Link className="p-2" href="/cart">Cart</Link>
+                            <Link className="p-2" href="/login">Login</Link>
+                        </div>
+                    </nav>
+                </header>
+                <main className="container m-auto mt-4 px-4">{children}</main>
+                <footer className="flex justify-center items-center h-10">Footer</footer>
             </div>
         </>
     );
