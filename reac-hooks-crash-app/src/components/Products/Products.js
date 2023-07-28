b807ac0f8d64d24d9418f1d829ebdcba07e158fa
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-28 11:07:05 +0330

    work useReducer

--- reac-hooks-crash-app/src/components/Products/Products.js
+++ reac-hooks-crash-app/src/components/Products/Products.js
@@ -1,13 +1,27 @@
-import React, { useState, useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 import ProductForm from "./ProductForm";
 import Search from "./Search";
 import ProductList from "./ProductList";
 
+const productReducer = (state, action) => {
+    switch (action.type) {
+        case "SET":
+            return action.products;
+        case "ADD":
+            return [...state, action.product];
+        default:
+            throw new Error("Error");
+    }
+};
+
 function Products() {
-    const [products, setProducts] = useState([]);
+    // const [products, setProducts] = useState([]);
+
+    const [products, dispatch] = useReducer(productReducer, []);
 
     const searchProductsHandler = useCallback((items) => {
-        setProducts(items);
+        // setProducts(items);
+        dispatch({ type: "SET", products: items });
     }, []);
 
     const addProductHandler = (newProduct) => {
@@ -17,11 +31,15 @@ function Products() {
             headers: { "Content-Type": "application/json" },
         }).then((response) => {
             response.json().then((responseData) => {
-                setProducts((prevState) => {
-                    return [
-                        ...prevState,
-                        { id: responseData.name, ...newProduct },
-                    ];
+                // setProducts((prevState) => {
+                //     return [
+                //         ...prevState,
+                //         { id: responseData.name, ...newProduct },
+                //     ];
+                // });
+                dispatch({
+                    type: "ADD",
+                    product: { id: responseData.name, ...newProduct },
                 });
             });
         });
