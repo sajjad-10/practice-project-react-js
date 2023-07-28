2214fab7ae0212e61d86f04d4da2afd5d467d3fe
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-28 17:06:53 +0330

    shopping cart context

--- shopping-next-app/pages/_app.js
+++ shopping-next-app/pages/_app.js
@@ -1,9 +1,12 @@
+import CartContextProvider from "@/context/Cart";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
     return (
         <div className="bg-gray-100">
-            <Component {...pageProps} />
+            <CartContextProvider>
+                <Component {...pageProps} />
+            </CartContextProvider>
         </div>
     );
 }
