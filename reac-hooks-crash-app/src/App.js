1eef56462d527e018a558f782b218820928be451
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-28 12:37:20 +0330

    using useContext

--- reac-hooks-crash-app/src/App.js
+++ reac-hooks-crash-app/src/App.js
@@ -1,8 +1,17 @@
+import React,{useContext} from "react";
+import { AuthContext } from "./context/auth-context";
 import Products from "./components/Products/Products";
+import Auth from "./components/Auth";
 import "./App.css";
 
 function App() {
-    return <Products />;
+    const authContext = useContext(AuthContext)
+
+    let content = <Auth/>
+    if (authContext.isAuth) {
+     content = <Products/>
+    }
+    return content;
 }
 
 export default App;
