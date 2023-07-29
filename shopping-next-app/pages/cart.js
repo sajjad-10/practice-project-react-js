2a70493e579556511af26253cf1741c615dce709
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-29 12:12:54 +0330

    display cart item

--- shopping-next-app/pages/cart.js
+++ shopping-next-app/pages/cart.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Layout from "@/components/Layout";
 import { useContext } from "react";
 import { CartContext } from "@/context/Cart";
@@ -14,7 +15,52 @@ const CartPage = () => {
                 {cartItems.length === 0 ? (
                     <div>Cart is empty.</div>
                 ) : (
-                    <div>Cart</div>
+                    <div className="grid md:grid-cols-4 md:gap-5">
+                        <div className="overflow-x-auto md:col-span-3">
+                            <table className="min-w-full">
+                                <thead className="border-b">
+                                    <tr>
+                                        <th className="px-5 text-left">Item</th>
+                                        <th className="px-5 text-right">
+                                            Quantity
+                                        </th>
+                                        <th className="px-5 text-right">
+                                            Price
+                                        </th>
+                                        <th className="px-5">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cartItems.map((item) => (
+                                        <tr
+                                            key={item.slug}
+                                            className="border-b"
+                                        >
+                                            <td>
+                                                <span className="flex items-center">
+                                                    <Image
+                                                        src={item.image}
+                                                        width={50}
+                                                        height={50}
+                                                    />
+                                                    {item.title}
+                                                </span>
+                                            </td>
+                                            <td className="p-5 text-right">
+                                                {item.qty}
+                                            </td>
+                                            <td className="p-5 text-right">
+                                                {item.price}
+                                            </td>
+                                            <td className="p-5 text-center">
+                                                <button>Remove</button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 )}
             </Layout>
         </>
