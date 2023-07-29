e5649eaee2604b5916d308bae6f315337ecac6cf
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-07-29 09:04:20 +0330

    show to cart items

--- shopping-next-app/components/Layout.js
+++ shopping-next-app/components/Layout.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useContext } from "react";
+import { CartContext } from "@/context/Cart";
 const Layout = ({ children, title }) => {
+    const { state, dispatch } = useContext(CartContext);
+    const { cart } = state;
     return (
         <>
             <Head>
@@ -9,15 +13,31 @@ const Layout = ({ children, title }) => {
             <div className="flex min-h-screen flex-col justify-between">
                 <header>
                     <nav className="flex h-14 px-8 justify-between items-center border-b-4 bg-white">
-                        <Link href={"/"} className="text-lg font-bold">Shopping</Link>
+                        <Link href={"/"} className="text-lg font-bold">
+                            Shopping
+                        </Link>
                         <div>
-                            <Link className="p-2" href="/cart">Cart</Link>
-                            <Link className="p-2" href="/login">Login</Link>
+                            <Link className="p-2" href="/cart">
+                                Cart
+                                {cart.cartItems.length > 0 && (
+                                    <span className="ml-1 rounded-xl bg-gray-200 px-2 py-1 text-xs font-bold">
+                                        {cart.cartItems.reduce(
+                                            (a, c) => a + c.qty,
+                                            0
+                                        )}
+                                    </span>
+                                )}
+                            </Link>
+                            <Link className="p-2" href="/login">
+                                Login
+                            </Link>
                         </div>
                     </nav>
                 </header>
                 <main className="container m-auto mt-4 px-4">{children}</main>
-                <footer className="flex justify-center items-center h-10">Footer</footer>
+                <footer className="flex justify-center items-center h-10">
+                    Footer
+                </footer>
             </div>
         </>
     );
