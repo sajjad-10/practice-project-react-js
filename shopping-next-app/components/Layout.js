e8898b7bcfbe4f30ade11ca598c73ecf79aba547
sajjad-10 <sajjadfallahi.official@gmail.com>
2023-08-03 09:50:30 +0330

    update items in header

--- shopping-next-app/components/Layout.js
+++ shopping-next-app/components/Layout.js
@@ -1,10 +1,15 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { CartContext } from "@/context/Cart";
 const Layout = ({ children, title }) => {
     const { state, dispatch } = useContext(CartContext);
     const { cart } = state;
+    const [cartItemsCount, setCartItemsCount] = useState(0);
+
+    useEffect(() => {
+        setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.qty, 0));
+    }, [cart.cartItems]);
     return (
         <>
             <Head>
@@ -21,10 +26,7 @@ const Layout = ({ children, title }) => {
                                 Cart
                                 {cart.cartItems.length > 0 && (
                                     <span className="ml-1 rounded-xl bg-gray-200 px-2 py-1 text-xs font-bold">
-                                        {cart.cartItems.reduce(
-                                            (a, c) => a + c.qty,
-                                            0
-                                        )}
+                                        {cartItemsCount}
                                     </span>
                                 )}
                             </Link>
